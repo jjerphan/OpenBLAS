0ca2b1539579fe1209ab84b34e5b33e9e5b6e393
Ian Thomas <ianthomas23@gmail.com>
2025-12-08 17:10:40 +0000

    Enable LAPACK_FORTRAN_STDLEN_END for lapacke calling lapack
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>

--- lapack-netlib/LAPACKE/include/lapack.h
+++ lapack-netlib/LAPACKE/include/lapack.h
@@ -17,9 +17,9 @@
 /* It seems all current Fortran compilers put strlen at end.
 *  Some historical compilers put strlen after the str argument
 *  or make the str argument into a struct. */
-#ifndef __EMSCRIPTEN__
+
 #define LAPACK_FORTRAN_STRLEN_END
-#endif
+
 
 #ifndef FORTRAN_STRLEN
   #define FORTRAN_STRLEN size_t
